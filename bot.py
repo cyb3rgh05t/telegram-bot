2b765efdc395c96fa78deae2f6255fbd44752dd6
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2024-09-27 14:56:00 +0200

    fix on ``load_group_id``

--- bot.py
+++ bot.py
@@ -1023,7 +1023,7 @@ async def main() -> None:
            #Log the successful retrieval of the token and timezone
            configure_bot(TOKEN, TIMEZONE="Europe/Berlin")
 
-           set_group_id()
+           load_group_id()
 
            init_db()
 
