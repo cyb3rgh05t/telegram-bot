9bf40a4ba84620cabf985186e78d7ea0c25341dd
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2024-10-06 19:55:25 +0200

    static files root folder fix

--- panel/panel/settings.py
+++ panel/panel/settings.py
@@ -144,6 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Adjust based on your app structure
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This should be a string path
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
