5765043bbc58fa2c102d7ac88f0865fff6c4273b
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2024-10-15 20:16:48 +0200

    nginx fix

--- panel/panel/settings.py
+++ panel/panel/settings.py
@@ -139,11 +139,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL =
+STATIC_URL = STATIC_URL
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media configuration for handling uploaded files
-MEDIA_URL = 
+MEDIA_URL = MEDIA_URL
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
