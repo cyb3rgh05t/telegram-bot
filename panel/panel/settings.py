bd237ef8f25a5c80850f2fbf6c8621eb704f8af6
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2024-10-15 19:14:05 +0200

    version

--- panel/panel/settings.py
+++ panel/panel/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media configuration for handling uploaded files
 MEDIA_URL = "/media/"
--- panel/post_manager/apps.py
+++ panel/post_manager/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PostManagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'post_manager'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "post_manager"
